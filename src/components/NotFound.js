f1be29a2d8d42efe9914e41c774afd5304f35ad1
stephane mallaroni <stephane.mallaroni@sonergia.fr>
2019-05-17 09:31:00 +0200

    - ajout des fichiers pas encore ajoutés au projet plus mise en place controle sur format email

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+/**
+ * Created by stephane.mallaroni on 15/04/2019.
+ */
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render(){
+        return(
+            <h1>Page non trouvée !</h1>
+        )
+    }
+}
+
+export default NotFound;
---
+++ src/variables.js
@@ -0,0 +1,5 @@
+/**
+ * Created by stephane.mallaroni on 12/04/2019.
+ */
+//export const API_PATH = 'http://apisiret/api_siret/';
+export const API_PATH = 'http://127.0.0.1:8000/api_lumen/';
