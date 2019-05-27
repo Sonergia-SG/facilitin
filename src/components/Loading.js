cb7f074f51043faac1fc04a2c35067a023a88729
Igor ESCHALIER <igor.eschalier@gmail.com>
2019-05-27 16:39:48 +0200

    fix Loading eslint errors
    > No state required so transform it in functionnal component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,38 +1,32 @@
 /**
  * Created by stephane.mallaroni on 17/04/2019.
  */
-import React, { Component } from 'react';
+import React from 'react';
 import Loader from 'react-loader-spinner';
+import PropTypes from 'prop-types';
 
-class Loading extends Component {
-  state = {
-    show: this.props.show === undefined ? false : this.props.show,
-    typeprops: this.props.type === undefined ? 'Puff' : this.props.type,
+const Loading = ({ show, type }) => {
+  if (show) {
+    return (
+      <Loader
+        type={type}
+        color="#EE6D42"
+        height="110"
+        width="110"
+      />
+    );
   }
+  return (<span />);
+};
 
-  componentDidUpdate(prevProps) {
-    const { show } = this.props;
-    // Typical usage (don't forget to compare props):
-    if (show !== prevProps.show) {
-      // ! this.setState in didUpate is dangerous
-      this.setState({ show });
-    }
-  }
+Loading.propTypes = {
+  show: PropTypes.bool,
+  type: PropTypes.string,
+};
 
-  render() {
-    const { show, typeprops } = this.state;
-    if (show) {
-      return (
-        <Loader
-          type={typeprops}
-          color="#EE6D42"
-          height="110"
-          width="110"
-        />
-      );
-    }
-    return (<span />);
-  }
-}
+Loading.defaultProps = {
+  show: false,
+  type: 'Puff',
+};
 
 export default Loading;
