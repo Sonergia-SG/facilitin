af9fb18fbf60ab7fb26798744f08fde9e8955d73
Igor ESCHALIER <igor.eschalier@gmail.com>
2019-05-27 17:12:14 +0200

    Fix some eslint issues with DropZone
    - init state in class property style
    - spread operator to update state.files
    - prettier

--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -3,68 +3,71 @@
  */
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
+import PropTypes from 'prop-types';
 
 class DropZone extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id_file: this.props.id_file,
-      files: [],
-    };
+  state = {
+    id_file: this.props.id_file,
+    files: [],
+  };
+
+  onDrop = (acceptedFiles) => {
+    // eslint-disable-next-line no-unused-vars, camelcase
+    const { id_file } = this.state;
+
+    this.setState({
+      files: {
+        ...acceptedFiles,
+        id_file,
+      },
+    });
+  };
+
+  render() {
+    const maxSize = 5242880;
+    const { files } = this.state;
+
+    return (
+      <div className="text-center mt-5">
+        <Dropzone
+          onDrop={this.onDrop}
+          accept="application/pdf, application/msword, application/vnd.oasis.opendocument.text"
+          minSize={0}
+          maxSize={maxSize}
+          multiple={false}
+        >
+          {({
+            getRootProps, getInputProps, isDragActive, isDragReject, rejectedFiles,
+          }) => {
+            const isFileTooLarge = rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+            return (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <span className="bigplus">
+                  <i className="fas fa-file-upload fa-2x" />
+                </span>
+                <br />
+                {isDragActive && !isDragReject ? 'Déposez votre fichier ici !' : ''}
+                {isDragReject && 'Type de fichier non accepté !'}
+                {isFileTooLarge && (
+                  <div className="text-danger mt-2">Le fichier est trop volumineux (5Mb Max).</div>
+                )}
+              </div>
+            );
+          }}
+        </Dropzone>
+        {files.map(f => (
+          <div className="notification is-primary notif-file" key={f}>
+            {f.name}
+          </div>
+        ))}
+      </div>
+    );
   }
-
-    onDrop = (acceptedFiles) => {
-      Object.defineProperty(acceptedFiles, 'id_file', {
-        value: this.state.id_file,
-      });
-
-      this.setState({
-        files: acceptedFiles,
-      });
-    }
-
-
-    render() {
-      const maxSize = 5242880;
-      return (
-        <div className="text-center mt-5">
-          <Dropzone
-            onDrop={this.onDrop}
-            accept="application/pdf, application/msword, application/vnd.oasis.opendocument.text"
-            minSize={0}
-            maxSize={maxSize}
-            multiple={false}
-          >
-            {({
-              getRootProps, getInputProps, isDragActive, isDragReject, rejectedFiles,
-            }) => {
-              const isFileTooLarge = rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
-              return (
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  <span className="bigplus"><i className="fas fa-file-upload fa-2x" /></span>
-                  <br />
-                  {isDragActive && !isDragReject ? 'Déposez votre fichier ici !' : ''}
-                  {isDragReject && 'Type de fichier non accepté !'}
-                  {isFileTooLarge && (
-                  <div className="text-danger mt-2">
-                                        Le fichier est trop volumineux (5Mb Max).
-                  </div>
-                  )}
-                </div>
-              );
-            }}
-          </Dropzone>
-          {
-                    this.state.files.map(f => (
-                      <div className="notification is-primary notif-file" key={f}>
-                        {f.name}
-                      </div>
-                    ))
-                }
-        </div>
-      );
-    }
 }
+
+DropZone.propTypes = {
+  id_file: PropTypes.string.isRequired,
+};
 
 export default DropZone;
