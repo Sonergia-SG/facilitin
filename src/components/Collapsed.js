53e4c15153ea64d6126110113ef6c6939bddb8ff
Igor Eschalier <igor.eschalier@sonergia.local>
2019-05-28 15:08:13 +0200

    Fix eslint issues for Collapsed component
    - remove useless state
    - keep care of index key

--- src/components/Collapsed.js
+++ src/components/Collapsed.js
@@ -2,35 +2,34 @@
  * Created by stephane.mallaroni on 15/04/2019.
  */
 import React, { Component } from 'react';
-import bulmaAccordion from 'bulma-accordion/dist/js/bulma-accordion.js';
+import bulmaAccordion from 'bulma-accordion/dist/js/bulma-accordion';
 import 'bulma-accordion/dist/css/bulma-accordion.min.css';
+import PropTypes from 'prop-types';
+
 import Accordion from './Accordion';
 
 class Collapsed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      accordions: [],
-      documents: this.props.valeur,
-    };
-  }
-
   componentDidMount() {
-    this.setState({ accordions: bulmaAccordion.attach() });
+    this.accordion = bulmaAccordion.attach();
   }
 
   render() {
-    let accordion = [];
-    accordion = this.state.documents.map((value, index) => <Accordion valeur={value} key={index} numero={index} />);
+    const { valeur } = this.props;
 
     return (
       <div>
         <section className="accordions">
-          {accordion}
+          {valeur.map((value, index) => (
+            <Accordion valeur={value} key={value.id_file} numero={index} />
+          ))}
         </section>
       </div>
     );
   }
 }
+
+Collapsed.propTypes = {
+  valeur: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
 
 export default Collapsed;
