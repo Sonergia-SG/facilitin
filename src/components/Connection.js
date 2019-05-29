06b45a6c0434933903e54270c0e9a4fafa419b2b
Igor Eschalier <igor.eschalier@sonergia.local>
2019-05-29 12:21:46 +0200

    Quick fix chrome autofill

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,7 +1,7 @@
 /**
  * Created by stephane.mallaroni on 11/04/2019.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -12,67 +12,85 @@ import Loading from './Loading';
 
 import { loginRequest, loginUpdateEmail, loginUpdatePassword } from '../store/actions/views/login';
 
-const Connection = ({
-  loginState, updateEmail, updatePassword, login,
-}) => (
-  <div className="Connection columns is-mobile is-centered is-vcentered">
-    <div className="box is-half has-text-centered">
-      <div className="has-text-centered content-loading">
-        <div id="loading_connect">
-          <Loading show={loginState.loading} type="Puff" />
-        </div>
-        <img src={logo} alt="logo sonergia" width="250" />
-      </div>
-      <div className="has-text-centered">
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            login();
-          }}
-        >
-          <div className="field">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Votre Email"
-              className="input"
-              value={loginState.email}
-              onChange={e => updateEmail(e.target.value)}
-              required
-            />
-            {loginState.errors.email && <p style={{ color: 'red' }}>{loginState.errors.email}</p>}
-          </div>
-          <div className="field">
-            <input
-              type="password"
-              id="mdp"
-              name="mdp"
-              placeholder="Votre Mot de passe"
-              className="input"
-              value={loginState.password}
-              onChange={e => updatePassword(e.target.value)}
-              required
-            />
-            {loginState.errors.password && (
-              <p style={{ color: 'red' }}>{loginState.errors.password}</p>
-            )}
-          </div>
-          <div className="field">
-            <div className="control">
-              <input
-                type="submit"
-                className="button is-primary is-outlined is-fullwidth"
-                value="Connexion"
-              />
+class Connection extends Component {
+  componentDidMount = () => {
+    if (this.emailRef) {
+      setTimeout(() => {
+        this.emailRef.focus();
+      }, 100);
+    }
+  }
+
+  render() {
+    const {
+      loginState, updateEmail, updatePassword, login,
+    } = this.props;
+    return (
+      <div className="Connection columns is-mobile is-centered is-vcentered">
+        <div className="box is-half has-text-centered">
+          <div className="has-text-centered content-loading">
+            <div id="loading_connect">
+              <Loading show={loginState.loading} type="Puff" />
             </div>
+            <img src={logo} alt="logo sonergia" width="250" />
           </div>
-        </form>
-        {loginState.errors.form && <p style={{ color: 'red' }}>{loginState.errors.form}</p>}
+          <div className="has-text-centered">
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                login();
+              }}
+            >
+              <div className="field">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Votre Email"
+                  className="input"
+                  value={loginState.email}
+                  onChange={e => updateEmail(e.target.value)}
+                  ref={(ref) => {
+                    this.emailRef = ref;
+                  }}
+                  required
+                />
+                {loginState.errors.email && (
+                  <p style={{ color: 'red' }}>{loginState.errors.email}</p>
+                )}
+              </div>
+              <div className="field">
+                <input
+                  type="password"
+                  id="mdp"
+                  name="mdp"
+                  placeholder="Votre Mot de passe"
+                  className="input"
+                  value={loginState.password}
+                  onChange={e => updatePassword(e.target.value)}
+                  required
+                />
+                {loginState.errors.password && (
+                  <p style={{ color: 'red' }}>{loginState.errors.password}</p>
+                )}
+              </div>
+              <div className="field">
+                <div className="control">
+                  <input
+                    type="submit"
+                    className="button is-primary is-outlined is-fullwidth"
+                    value="Connexion"
+                  />
+                </div>
+              </div>
+            </form>
+            {loginState.errors.form && <p style={{ color: 'red' }}>{loginState.errors.form}</p>}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 Connection.propTypes = {
   login: PropTypes.func.isRequired,
