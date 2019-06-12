6049dcc6690d7eb17494efb45332781c414724bf
Igor Eschalier <igor.eschalier@sonergia.local>
2019-06-12 11:28:56 +0200

    Do not send sentry error in local env

--- src/tools/errorReporting/index.js
+++ src/tools/errorReporting/index.js
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/browser';
 
 Sentry.init({
-  dsn: 'https://6d128336cbe347aa8be18f00d28df9e8@sentry.io/1470574',
+  dsn: process.env.NODE_ENV
+    ? undefined
+    : 'https://6d128336cbe347aa8be18f00d28df9e8@sentry.io/1470574',
   debug: process.env.NODE_ENV === 'development',
   environment: process.env.NODE_ENV,
 });
