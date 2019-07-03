f1f5827414e2e8f42846bda9bc2a3238dca36d10
Igor ESCHALIER <igor.eschalier@sonergia.fr>
2019-07-03 08:43:11 +0200

    Footer open/close

--- src/Common/Footer/index.tsx
+++ src/Common/Footer/index.tsx
@@ -10,28 +10,36 @@ const Footer = () => {
 
   return (
     <div>
-      <div className="Footer-selector">
-        <button
-          className="button is-small"
-          type="button"
-          tabIndex={0}
-          onClick={() => setOpen(!open)}
-          onKeyPress={() => setOpen(!open)}
-        >
-          <span className={`icon is-small Footer-${icon}`}>
-            <i className="fas fa-chevron-up" />
-          </span>
-        </button>
-      </div>
-      <div className="Footer-container">
-        <div className="Footer-always-visible">
-          <img src={logo} alt="Sonergia logo" style={{ height: 40 }} />
-          <p>
-            {"Simplifie vos économies d'énergie & Donne de l'avenir à vos projets"}
-          </p>
-          <button className="button is-small is-outline" type="button">
-            Déclarer un bug
+      <div className="Footer-hacked-space" />
+      <div className="Footer-fixed">
+        <div className="Footer-selector">
+          <button
+            className="button is-small"
+            type="button"
+            tabIndex={0}
+            onClick={() => setOpen(!open)}
+            onKeyPress={() => setOpen(!open)}
+          >
+            <span className={`icon is-small Footer-selector-icon Footer-${icon}`}>
+              <i className="fas fa-chevron-up" />
+            </span>
           </button>
+        </div>
+        <div className="Footer-container">
+          <div className="Footer-always-visible">
+            <img src={logo} alt="Sonergia logo" style={{ height: 40 }} />
+            <p>
+              {"Simplifie vos économies d'énergie & Donne de l'avenir à vos projets"}
+            </p>
+            <button className="button is-small is-outline" type="button">
+              Déclarer un bug
+            </button>
+          </div>
+          {open && (
+            <div className="Footer-usefull-links">
+              Des liens utiles bientôt disponible ici
+            </div>
+          )}
         </div>
       </div>
     </div>
