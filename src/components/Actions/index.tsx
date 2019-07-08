4960a729510c56e98184d5312ef3f876d15787d6
Igor ESCHALIER <igor.eschalier@sonergia.fr>
2019-07-08 16:19:59 +0200

    code clean

--- src/components/Actions/index.tsx
+++ src/components/Actions/index.tsx
@@ -5,9 +5,6 @@ import React, { Component } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { denormalize } from 'normalizr';
-// @ts-ignore
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
 import {
   Tab,
   Tabs,
@@ -17,7 +14,6 @@ import {
 } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
-import StatusCell from './StatusCell';
 import Loading from '../Loading';
 
 import ModernTable from './ModernTable';
@@ -36,58 +32,6 @@ import { AppState } from '../../store';
 import { Entities, OperationFull } from '../../store/reducer/entities/types';
 import { ListState, Tab as TabType } from '../../store/reducer/views/list/type';
 import { UserFonction } from '../../store/reducer/user/types';
-
-const COLUMNS = [
-  {
-    Header: 'N° Dossier',
-    id: 'id_dossierprime',
-    accessor: (d: OperationFull) => d.id_dossierprime, // String-based value accessors!
-  },
-  {
-    Header: 'N° Action',
-    id: 'id_dp_operation',
-    accessor: (d: OperationFull) => d.id_dp_operation,
-    // Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-  },
-  {
-    Header: 'Etat',
-    id: 'label_public',
-    accessor: (d: OperationFull) => (d.statut ? d.statut.label_public : ' - '),
-  },
-  {
-    Header: 'Nb Jours res',
-    id: 'delai_instruction',
-    accessor: (d: OperationFull) => d.moderemuneration.delai_instruction,
-  },
-  {
-    Header: 'MOA',
-    id: 'moa_nom',
-    accessor: (d: OperationFull) => (d.dossierprime
-      ? `${d.dossierprime.moa_nom} ${d.dossierprime.moa_prenom} ${
-        d.dossierprime.moa_denomination
-      }`
-      : ' - '),
-  },
-  {
-    Header: 'FOST',
-    id: 'code_operation',
-    accessor: (d: OperationFull) => d.code_operation,
-  },
-  {
-    Header: ' ',
-    id: 'stats',
-    accessor: 'TODO',
-    Cell: StatusCell,
-  },
-];
-
-const TRANSLATIONS = {
-  previousText: 'Précédent',
-  nextText: 'Suivant',
-  loadingText: 'Chargement...',
-  ofText: 'sur',
-  rowsText: 'lignes',
-};
 
 interface Props extends RouteComponentProps {
   loadList: any;
@@ -130,7 +74,7 @@ class Actions extends Component<Props> {
       : mappedData;
 
     return (
-      <div style={{ backgroundColor: '#fff', padding: '10px 20px', marginBottom: 30 }}>
+      <div style={{ backgroundColor: '#f1f2f7', padding: '10px 20px', marginBottom: 30 }}>
         <div className="has-text-centered content-loading">
           <div id="loading_liste">
             <Loading show={loading} type="ThreeDots" />
