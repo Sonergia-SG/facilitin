285c2e5a229209ecc639e50c0984df954baae369
Igor ESCHALIER <igor.eschalier@sonergia.fr>
2019-07-11 15:23:06 +0200

    Handle all existing action status color

--- src/components/Folder/Left/Status.tsx
+++ src/components/Folder/Left/Status.tsx
@@ -6,11 +6,48 @@ interface Props {
   status?: OperationStatus;
 }
 
+const backgroundColor = (status?: number) => {
+  switch (status) {
+    case -40:
+      return '#7d8433';
+    case -5:
+      return '#ff7aff';
+    case -30:
+    case 1:
+    case 3:
+    case 0:
+      return '#57c8f1';
+    case 5:
+    case -10:
+      return '#e4ba00';
+    case -15:
+    case 13:
+      return '#f9741b';
+    case 15:
+    case 16:
+    case 17:
+    case 2:
+      return '#95b75d';
+    case 8:
+    case 10:
+    case 18:
+    case 21:
+      return '#ec6459';
+    case -20:
+    case 20:
+    default:
+      return '#607d8b';
+  }
+};
+
 const Status = ({ status }: Props) => {
   if (!status) return null;
 
   return (
-    <p style={{ backgroundColor: status.code_couleur }} className="Left-GeneralInfos-Status">
+    <p
+      style={{ backgroundColor: backgroundColor(status.code_statut) }}
+      className="Left-GeneralInfos-Status"
+    >
       {status.label_public}
     </p>
   );
