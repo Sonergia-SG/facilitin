8481b84e69409beae9eb5eb4d254bc790851c5f8
Sofiane <sofiane.abidi@sonergia.fr>
2019-07-26 09:42:08 +0200

    DeleteFile.tsx

---
+++ src/components/Folder/Accordion/DeleteFile.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import rest from '../../../tools/rest';
+import { API_PATH } from '../../../variables';
+import { addMessageToQueue } from '../../Alert';
+import {
+  BooleanNumber,
+  SimpleFile,
+} from '../../../store/reducer/entities/types';
+
+import downloadDataUri from '../../../tools/file/downloadDataUri';
+
+interface Props {
+  file: SimpleFile;
+}
+
+class DeleteFile extends Component<Props> {
+  delete = async () => {
+    try {
+      const result = await rest(`${API_PATH}files/${this.props.file.id_file}`, {method: 'DELETE'});
+
+      if (result.status === 200) {
+        window.location.reload();
+      } else {
+        addMessageToQueue({
+          duration: 4000,
+          type: 'error',
+          message: 'Erreur pendant la suppression du fichier',
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      addMessageToQueue({
+        duration: 4000,
+        type: 'error',
+        message: 'Erreur pendant la suppression du fichier',
+      });
+    }
+  };
+
+  render() {
+    // @ts-ignore
+    if (this.props.file.id_file > 0) {
+      return (
+        <div style={{ width: 20, margin: '0 3px' }}>
+          <div
+            onClick={this.delete}
+            onKeyPress={this.delete}
+            style={{ cursor: 'pointer' }}
+            role="button"
+            tabIndex={0}
+          >
+            <i style={{ fontSize: 24 }} className="fas fa-trash" />
+          </div>
+        </div>
+      );
+    }
+
+    return (
+    // @ts-ignore
+      <div style={{ width: 20, margin: '0 3px', opacity: '0.6' }}>
+        <div
+          tabIndex={0}
+        >
+          <i style={{ fontSize: 24 }} className="fas fa-file-download" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DeleteFile;
