70c8dc3c834d1fc8dd8f06107b6904bc8525b79a
Igor ESCHALIER <igor.eschalier@sonergia.fr>
2019-08-05 16:15:59 +0200

    fix(document): use Different way to download files
    this way seems to be accept by chrome and ie

--- src/tools/file/downloadDataUri.tsx
+++ src/tools/file/downloadDataUri.tsx
@@ -1,7 +1,19 @@
-const downloadDataURI = (uri: string, name: string) => {
+const downloadDataURI = (uri: string, name: string, mimetype: string) => {
+  const bytes = atob(uri);
+
+  const byteNumbers = new Array(bytes.length);
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < bytes.length; i++) {
+    byteNumbers[i] = bytes.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: mimetype });
+  const url = window.URL.createObjectURL(blob);
+
   const link = document.createElement('a');
   link.download = name;
-  link.href = uri;
+  link.href = url;
+  link.target = '_blank';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
