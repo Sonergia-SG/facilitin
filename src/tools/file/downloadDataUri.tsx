291f51e107310d930c26420b8b58d3aceae42b21
Igor ESCHALIER <igor.eschalier@sonergia.fr>
2019-08-06 10:09:18 +0200

    be functional

--- src/tools/file/downloadDataUri.tsx
+++ src/tools/file/downloadDataUri.tsx
@@ -1,12 +1,8 @@
 const downloadDataURI = (uri: string, name: string, mimetype: string) => {
   const bytes = atob(uri);
-
-  const byteNumbers = new Array(bytes.length);
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < bytes.length; i++) {
-    byteNumbers[i] = bytes.charCodeAt(i);
-  }
+  const byteNumbers = bytes.split('').map(c => c.charCodeAt(0));
   const byteArray = new Uint8Array(byteNumbers);
+
   const blob = new Blob([byteArray], { type: mimetype });
   const url = window.URL.createObjectURL(blob);
 
