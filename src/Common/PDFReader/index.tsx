fb50cf4fdd8300e00b9a8afca7bc42eece836cc4
Igor ESCHALIER <igor.eschalier@sonergia.fr>
2019-08-14 09:14:59 +0200

    feat(preview): handle loading/error
    > Fix refetch file after upload bug

--- src/Common/PDFReader/index.tsx
+++ src/Common/PDFReader/index.tsx
@@ -10,6 +10,7 @@ import { BooleanNumber } from '../../store/reducer/entities/types';
 import { onlyDataArray } from '../../tools/file/downloadDataUri';
 
 import './PDFReader.css';
+import Empty from '../../components/Folder/Empty';
 
 pdfjs.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.1.266/pdf.worker.js';
 
@@ -22,6 +23,7 @@ const PDFReader = ({ idFile }: { idFile: number }) => {
   const [page, setPage] = useState(1);
   const [max, setMax] = useState(1);
   const [scale, setScale] = useState(1);
+  const [dlState, setDlState] = useState({ loading: 0 });
 
   const updateScale = async () => {
     if (containerRef.current && viewportRef.current) {
@@ -81,38 +83,44 @@ const PDFReader = ({ idFile }: { idFile: number }) => {
   });
 
   const dl = async () => {
-    const result = await rest(`${API_PATH}files/${idFile}`);
+    try {
+      setDlState({ loading: 1 });
+      const result = await rest(`${API_PATH}files/${idFile}`);
 
-    if (result.status === 200) {
-      interface JSON {
-        status: 'success' | 'fail';
-        file: {
-          id_file: number;
-          datecreation: string;
-          id_user: number;
-          shorturl: string | null;
-          filename: string;
-          mimetype: string;
-          exported: BooleanNumber;
-          no_content: BooleanNumber;
-          deleted: BooleanNumber;
-          file_binary: {
+      if (result.status === 200) {
+        interface JSON {
+          status: 'success' | 'fail';
+          file: {
             id_file: number;
-            binarycontent: string;
+            datecreation: string;
+            id_user: number;
+            shorturl: string | null;
+            filename: string;
+            mimetype: string;
+            exported: BooleanNumber;
+            no_content: BooleanNumber;
+            deleted: BooleanNumber;
+            file_binary: {
+              id_file: number;
+              binarycontent: string;
+            };
           };
-        };
+        }
+        const json: JSON = await result.json();
+
+        const { file } = json;
+        const data = onlyDataArray(file.file_binary.binarycontent);
+        const task = pdfjs.getDocument(data);
+
+        pdfRef.current = await task.promise;
+        setDlState({ loading: 2 });
+
+        setMax(pdfRef.current.numPages);
+
+        renderPage(1, true);
       }
-      const json: JSON = await result.json();
-
-      const { file } = json;
-      const data = onlyDataArray(file.file_binary.binarycontent);
-      const task = pdfjs.getDocument(data);
-
-      pdfRef.current = await task.promise;
-
-      setMax(pdfRef.current.numPages);
-
-      renderPage(1, true);
+    } catch (error) {
+      setDlState({ loading: -1 });
     }
   };
 
@@ -120,45 +128,49 @@ const PDFReader = ({ idFile }: { idFile: number }) => {
     dl();
   }, []);
 
-  return (
-    <div className="PDFReader-container" ref={containerRef}>
-      <div className="PDFReader-pdf-container">
-        <canvas ref={canvasRef} />
-      </div>
-      <div>
-        <div className="PDFReader-page-control">
-          <button type="button" disabled={page === 1} onClick={() => renderPage(page - 1)}>
-            <i style={{ marginRight: 3 }} className="fas fa-caret-left fa-2x" />
-          </button>
-          <p>
-            page :
-            {' '}
-            {page}
-            {' '}
-/
-            {' '}
-            {max}
-          </p>
-          <button type="button" disabled={max < page + 1} onClick={() => renderPage(page + 1)}>
-            <i style={{ marginLeft: 3 }} className="fas fa-caret-right fa-2x" />
-          </button>
+  if (dlState.loading === 2) {
+    return (
+      <div className="PDFReader-container" ref={containerRef}>
+        <div className="PDFReader-pdf-container">
+          <canvas ref={canvasRef} />
         </div>
-        <div className="PDFReader-zoom-control">
-          <button
-            type="button"
-            style={{ marginBottom: 4 }}
-            disabled={scale >= 3}
-            onClick={() => setScale(scale + 0.2)}
-          >
-            <i style={{ marginRight: 0 }} className="fas fa-plus fa-2x" />
-          </button>
-          <button type="button" disabled={scale <= 0.4} onClick={() => setScale(scale - 0.2)}>
-            <i style={{ marginRight: 0 }} className="fas fa-minus fa-2x" />
-          </button>
+        <div>
+          <div className="PDFReader-page-control">
+            <button type="button" disabled={page === 1} onClick={() => renderPage(page - 1)}>
+              <i style={{ marginRight: 3 }} className="fas fa-caret-left fa-2x" />
+            </button>
+            <p>
+              page :
+              {' '}
+              {page}
+              {' '}
+  /
+              {' '}
+              {max}
+            </p>
+            <button type="button" disabled={max < page + 1} onClick={() => renderPage(page + 1)}>
+              <i style={{ marginLeft: 3 }} className="fas fa-caret-right fa-2x" />
+            </button>
+          </div>
+          <div className="PDFReader-zoom-control">
+            <button
+              type="button"
+              style={{ marginBottom: 4 }}
+              disabled={scale >= 3}
+              onClick={() => setScale(scale + 0.2)}
+            >
+              <i style={{ marginRight: 0 }} className="fas fa-plus fa-2x" />
+            </button>
+            <button type="button" disabled={scale <= 0.4} onClick={() => setScale(scale - 0.2)}>
+              <i style={{ marginRight: 0 }} className="fas fa-minus fa-2x" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <Empty loading={dlState.loading !== -1} />;
 };
 
 export default PDFReader;
--- src/components/Folder/Accordion/Preview.tsx
+++ src/components/Folder/Accordion/Preview.tsx
@@ -4,8 +4,6 @@ import idx from 'idx';
 
 import { SimpleFile } from '../../../store/reducer/entities/types';
 
-import useDownloadPreview from './useDownloadPreview';
-import Empty from './Empty';
 import { AppState } from '../../../store';
 import PDFReader from '../../../Common/PDFReader';
 
@@ -17,23 +15,16 @@ interface Props extends ConnectProps {
   uploadLoading: boolean | undefined;
 }
 
-// const { data, loading } = useDownloadPreview(file, !!uploadLoading);
-// if (loading === 0) return null;
 const Preview = ({ file, uploadLoading }: Props) => (
   <div style={{ width: '100%', height: '100%' }}>
-    {/* {data !== '' ? ( */}
-    {/* {file.id_file ? ( */}
-    {file.id_file !== null && <PDFReader idFile={file.id_file} />}
-    {/* ) : ( */}
-    {/* <Empty loading={loading === 1} /> */}
-    {/* )} */}
+    {file.id_file !== null && uploadLoading === false && <PDFReader idFile={file.id_file} />}
   </div>
 );
 export default connect((s: AppState, p: ConnectProps) => {
   if (p.file.id_file === null) return { loading: false };
 
   const { pending } = s.views.folder;
-  const fileId = p.file.id_file;
+  const fileId = p.file.id_dp_file;
   return {
     uploadLoading: idx(pending, _ => _[p.file.id_dp_operation].file[fileId].loading) || false,
   };
